89eb3a046bf25d5c1aecf8709549121d907014e0
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2024-10-25 15:39:31 -0500

    style: add colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,15 @@ export default {
   theme: {
     extend: {
       colors: {
-        brown: "#2f0601", // Background color
+        brown: "#1e140a", // Background color
         taupe: "#8a817c", // Text / secondary background
         gray: "#bcb8b1", // Another option for lighter sections
         cream: "#f4f3ee", // Light text color
         blush: "#e0afa0", // Accent color for links, hover, etc.
+      },
+      fontFamily: {
+        sans: ["Karla", "sans-serif"], // Default font
+        italiana: ["Italiana", "sans-serif"], // Headings or specific elements
       },
     },
   },
