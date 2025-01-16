b40f41eece71c3aa611c90d36a7cae0551b1a898
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2025-01-16 11:19:43 -0600

    refactor: clean up

--- src/components/TicTacToe.tsx
+++ src/components/TicTacToe.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-type SquareValue = "X" | "O" | null;
+type SquareType = "X" | "O" | null;
 
 const winCombos = [
   [0, 1, 2],
@@ -13,8 +13,8 @@ const winCombos = [
   [2, 4, 6],
 ];
 
-const calculateWinner = (squares: SquareValue[]): SquareValue => {
-  for (const [a,b,c] of winCombos) {
+const calculateWinner = (squares: SquareType[]): SquareType => {
+  for (const [a, b, c] of winCombos) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
@@ -22,69 +22,75 @@ const calculateWinner = (squares: SquareValue[]): SquareValue => {
   return null;
 };
 
-export const TicTacToe: React.FC = () => {
-  const [squares, setSquares] = useState<SquareValue[]>(Array(9).fill(null));
+export const TicTacToe = () => {
+  const [squares, setSquares] = useState<SquareType[]>(Array(9).fill(null));
   const [isXNext, setisXNext] = useState<boolean>(true);
 
   const winner = calculateWinner(squares);
+
+  const Square = (i: number) => (
+    <button
+      style={{
+        width: "50px",
+        height: "50px",
+        border: "2px solid #999",
+        margin: "5px",
+        fontSize: "24px",
+        fontWeight: "bold",
+      }}
+      onClick={() => handleClick(i)}
+    >
+      {squares[i]}
+    </button>
+  );
 
   const handleClick = (i: number) => {
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
-    const newSquares = squares.slice();
+
+    const newSquares = squares.slice(0);
     newSquares[i] = isXNext ? "X" : "O";
     setSquares(newSquares);
     setisXNext(!isXNext);
   };
 
-  const Square = (i: number) => {
-    return (
-      <button
-        style={{
-          width: "50px",
-          height: "50px",
-          fontSize: "24px",
-          fontWeight: "bold",
-          margin: "4px",
-          border: "2px solid #999",
-        }}
-        onClick={() => {
-          handleClick(i);
-        }}
-      >
-        {squares[i]}
-      </button>
-    );
-  };
-
+  //show status
   const status = winner
     ? `Winner: ${winner}`
     : squares.every(Boolean)
-    ? "Draw!"
-    : `Next player: ${isXNext ? "X" : "O"}`;
+    ? "Draw"
+    : `Next Player: ${isXNext ? "X" : "O"}`;
 
   return (
     <div
-      style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
     >
-      <div style={{ marginBottom: "10px", fontSize: "24px" }}>{status}</div>
-      <div>
-        <div style={{ display: "flex" }}>
-          {Square(0)}
-          {Square(1)}
-          {Square(2)}
-        </div>
-        <div style={{ display: "flex" }}>
-          {Square(3)}
-          {Square(4)}
-          {Square(5)}
-        </div>
-        <div style={{ display: "flex" }}>
-          {Square(6)}
-          {Square(7)}
-          {Square(8)}
-        </div>
+      <div
+        style={{
+          fontSize: "24px",
+        }}
+      >
+        {status}
+      </div>
+      <div style={{ display: "flex" }}>
+        {Square(0)}
+        {Square(1)}
+        {Square(2)}
+      </div>
+      <div style={{ display: "flex" }}>
+        {Square(3)}
+        {Square(4)}
+        {Square(5)}
+      </div>
+      <div style={{ display: "flex" }}>
+        {Square(6)}
+        {Square(7)}
+        {Square(8)}
       </div>
     </div>
   );
