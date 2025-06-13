00fdb6acdc62434bfbe48058c1753cfdbd4ef06a
Jasmine P. Vo <99847030+jasminepvo@users.noreply.github.com>
2025-06-13 14:54:01 -0500

    chore: remove unecessary addons

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,9 +7,6 @@ const config: StorybookConfig = {
   ],
   "addons": [
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
     {
       name: '@storybook/addon-a11y',
       options: {
